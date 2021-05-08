7e721eca439d5766d071a762a0f41747f517a08b
Naval <naval.surange@students.iiit.ac.in>
2021-05-08 20:38:03 +0530

    updated routing function
    --graph has to be updated for initial condition that is t=0

--- routing.c
+++ routing.c
@@ -8,9 +8,16 @@ void update_cars(struct Graph* g,int i,int time,StrHash hash){
     if(time=car[i].time_to_change && car[i].location_ptr<car[i].num_streets){ //if the car has reached the end of the edge and it still has more edge(s) to cover
         //**Naval->  update_edge(car[i].names_of_streets[location_ptr]) decrease by 1
         long long index = Find_StrHash(hash,car[i].names_of_streets[car[i].location_ptr]) ;
+
+        hash->bkt_arr[index].congestion--;
         decreaseCongestion(g,hash->bkt_arr[index].v1,hash->bkt_arr[index].v2);
+
         car[i].location_ptr++;
+
         //**Naval->  update_edge(car[i].names_of_streets[location_ptr]) increase by 1
+        index = Find_StrHash(hash,car[i].names_of_streets[car[i].location_ptr]) ;
+
+        hash->bkt_arr[index].congestion++;
         increaseCongestion(g,hash->bkt_arr[index].v1,hash->bkt_arr[index].v2);
         car[i].time_to_change+= calc_time((long long)hash->bkt_arr[index].congestion,(long long)hash->bkt_arr[index].length);/* obtain using car[i].names_of_streets[location_ptr])*/
     }
@@ -23,7 +30,10 @@ int update_myloc(struct Graph*g ,int time,int dest,StrHash hash){
         //me.curr_node= //update, obtain the current node from street end
         int currindex = Find_StrHash(hash,me.curr_street); 
         StrHash_NODE  a = hash->bkt_arr[currindex] ;
+
+        hash->bkt_arr[currindex].congestion--;
         decreaseCongestion(g,a.v1,a.v2);
+
         me.curr_node = a.v2;
 
         Stack s = dijikstra(g,me.curr_node,dest);
@@ -44,16 +54,29 @@ int update_myloc(struct Graph*g ,int time,int dest,StrHash hash){
         //update congestion values for both nodes 
         int next_index = Find_StrHash(hash,me.curr_street);
         StrHash_NODE next = hash->bkt_arr[next_index];
+        
         me.time_to_change+= calc_time(next.congestion,next.length);/* obtain using car[i].names_of_streets[location_ptr])*/
+        
+        hash->bkt_arr[next_index].congestion++;
         increaseCongestion(g,next.v1,next.v2);
+
         printf("\n The next route  you must take is : %s",me.curr_street);
         printf("\n Is the path ahead avalible to travel? (Y/N)");
-            scanf("%c",is_free);
-            if(is_free=='N' || 'n'){
-                //modify the suggested path edge weight->infinity
-                //dijikstra(me.curr_node,destination) // if no path available say->can't find any path 
-            }
-        printf("Our path is %s\n",next.str);
+
+        scanf("%c",is_free);
+        
+        // while (is_free=='N' || 'n')
+        // {
+        //     updateEdge(g,me.curr_node,me.end_node,10000);
+        //     dijikstra(g,a.v2,dest);
+
+        // }
+        
+            // if(is_free=='N' || 'n'){
+            //     //modify the suggested path edge weight->infinity
+            //     //dijikstra(me.curr_node,destination) // if no path available say->can't find any path 
+            // }
+        // printf("Our path is %s\n",next.str);
         return 1;
     }
     return 0;
