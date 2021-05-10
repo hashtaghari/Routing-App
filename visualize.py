ce346507e56fd80e97159a27e09df60dd1ed2c0d
Naval <naval.surange@students.iiit.ac.in>
2021-05-10 10:35:37 +0530

    highlighting of selected edge is working perfectly now

--- visualize.py
+++ visualize.py
@@ -4,42 +4,48 @@ import networkx as nx
 f = open(".\\data\\g2.txt", "r")
 f2 = open(".\\data\\curr_edge.txt", "r")
 
-name_edge = f2.readline()
-# print("\n in python name : "+name_edge)
-# name = "name1"
+Hi_edge_list = f2.readline().split()
+Hi_edge = Hi_edge_list[0]
+
+Hi_edge = str(Hi_edge)
+
+print("\n in python name : " + Hi_edge)
+
 
 highleted_color = "orange"
 back_ground = "#59999c"
-edge_color = "white"
+edge_color = "black"
 vertex_color = "#f67d7d"
 
 line1 = f.readline()
 G = nx.DiGraph()
-names = []
-color_list = []
+formatted_edge_labels_list = []
+
 for line in f:
     data = line.split()
-    # print(data)
-    G.add_edge(data[0], data[1], edge_labels=data[2], lenght=(1.33*len(data[2])))
-    if(data[2] == name_edge):
-        color_list.append(highleted_color)
+    if(str(data[2]) == Hi_edge):
+        G.add_edge(data[0], data[1], color=highleted_color, edge_label=data[2],
+                   lenght=(1.33*len(data[2])))
     else:
-        color_list.append(edge_color)
-    
-    names.append(((data[0], data[1]), data[2]))
+        G.add_edge(data[0], data[1], color=edge_color, edge_label=data[2],
+                   lenght=(1.33*len(data[2])))
+    formatted_edge_labels_list.append(((data[0],data[1]),data[2]))
+
+
+fig = plt.figure()
 
 pos = nx.circular_layout(G)
-fig = plt.figure()
-# edge_names = nx.get_edge_attributes(G,'edge_labels').values()
-nx.draw(G, pos, with_labels=True, node_size=450,
-        node_color=vertex_color, width=7, font_color='white')
 
+formatted_edge_labels = dict(formatted_edge_labels_list)
+colors = nx.get_edge_attributes(G, 'color').values()
 props = dict(boxstyle='round', facecolor="#FFFDD0", alpha=1)
 
-nx.draw_networkx_edge_labels(G, pos, edge_labels=dict(
-    names), font_color='#1C4755', bbox=props)
+nx.draw(G,pos,with_labels=True, font_color='white',width=7,edge_color=colors)
 
-nx.draw_networkx_edges(G, pos, edge_color=color_list, style="dashed")
+nx.draw_networkx_nodes(G,pos,node_size=450,node_color=vertex_color)
+nx.draw_networkx_edges(G, pos, edge_color="white",arrowsize=15,style="dashed")
+nx.draw_networkx_edge_labels(G,pos,edge_labels=formatted_edge_labels,font_color='red',bbox=props,label_pos=0.55)
+
 
 fig.set_facecolor(back_ground)
 plt.show()
