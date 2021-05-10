e54106aac19c759dc5bf1ffe7998097256ae167a
R-R-IIITH <rajarshi.ray@students.iiit.ac.in>
2021-05-10 16:08:26 +0300

    Edited.

--- string-hash.h
+++ string-hash.h
@@ -6,22 +6,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct str_hash_node
-{
+//Structure for creating a node of a hash table, used to represent a street.
+struct str_hash_node {
+    
+    //Name of street
     char str[31];
+    //To check if the street has been visited.
     bool visited;
+    //Length or distance of weight of the given street 
     int length;
+    //Start and end nodes or intersection of the street.
     int v1, v2;
+    //The congestion or the number of cars on the given street.
     int congestion;
 };
 
+//Renaming the structure.
 typedef struct str_hash_node StrHash_NODE;
 
-struct StrHash
-{
-    unsigned long int cap, curr;
+//Creating the structure for hash table.
+struct StrHash {
+
+    //The current capacity of the given hash table. It may change dynamically. 
+    unsigned long int cap;
+    //The current size of the given hash table.
+    unsigned long int curr;
+    //A pointer to hash table nodes.
     StrHash_NODE *bkt_arr;
 };
+
 typedef struct StrHash *StrHash;
 
 int len(char *str);
