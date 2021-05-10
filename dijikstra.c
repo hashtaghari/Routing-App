6e0976e24de57da88fee4a62229961b50898b78d
Naval <naval.surange@students.iiit.ac.in>
2021-05-10 18:47:43 +0530

    added comments in Dijikstra

--- dijikstra.c
+++ dijikstra.c
@@ -11,14 +11,18 @@ Stack dijikstra(struct Graph *graph, int src, int dest) {
     //Signifies the number of vertices in the graph.
     int V = graph->V;
 
-    //
+    //stores current minimum cost needed to reach all nodes from src
     int dist[V];
-    int sptset[V];
+    //stores parent of all the nodes(the node from which we arrived to the current node)
     int parent[V];
 
-    //The minHeap structure being formed so as to sort out the graph and its nodes to implement Djikstra's algorithm.
+    //minheap is used here to extract the minimum vertex, delete any vertex or change cost 
+    //to reach any vertex in log(n) time 
     struct MinHeap *minHeap = createMinHeap(V);
 
+    //initilizing and creating heap with distances to vertices
+    //(initiall all infinity since no path is known yet to reach there)
+    //initilizing parent[v] as each node is parent of itself initially
     for (int v = 0; v < V; ++v) {
 
         dist[v] = INT_MAX;
@@ -28,7 +32,10 @@ Stack dijikstra(struct Graph *graph, int src, int dest) {
 
     }
 
+    //making distance to starting node as 0 since it is reacheable from it self with zero cost
     dist[src] = 0;
+    //decreasing the distance of src node in heap
+    //also calls 
     decreaseKey(minHeap, src, dist[src]);
 
     minHeap->size = V;
